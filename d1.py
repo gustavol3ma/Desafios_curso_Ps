126c4a833374c9d621cc09cf7b54f004f0beaf9d
gustavol3ma <ggustavolimadm@gmail.com>
2025-05-01 01:01:57 -0300

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,6 @@
+nu01 = int(input('Escreva um número inteiro: '))
+nu02 = int(input('Escreva um segundo número inteiro: '))
+if nu01<nu02:
+    print("O maior número é o" ,nu02)
+else:
+    print("O maior número é o" ,nu01)
